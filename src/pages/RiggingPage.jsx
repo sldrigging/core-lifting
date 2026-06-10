0ea51e0023cc0fee2d39b89f74547678a6ddd45f
sldrigging <61851675+sldrigging@users.noreply.github.com>
2026-06-10 13:41:24 -0500

    Add form embed

--- src/pages/RiggingPage.jsx
+++ src/pages/RiggingPage.jsx
@@ -523,6 +523,9 @@ export default function RiggingPage() {
             </div>
           ))}
         </div>
+        <div>
+          <iframe width="640px" height="480px" src="https://forms.office.com/r/wssL87gKyR?embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+        </div>
       </section>
     </>
   );
