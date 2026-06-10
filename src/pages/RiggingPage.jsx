3a28717973535ef4e23d751b3d27d02d215f8cb2
sldrigging <61851675+sldrigging@users.noreply.github.com>
2026-06-10 14:24:55 -0500

    update form style

--- src/pages/RiggingPage.jsx
+++ src/pages/RiggingPage.jsx
@@ -527,13 +527,13 @@ export default function RiggingPage() {
       {/* Contact Form Section */}
 <section id="contact-form" style={{ display: 'flex', justifyContent: 'center', padding: '4rem 1rem', backgroundColor: '#f9f9f9' }}>
   <iframe 
-    width="640px" 
-    height="480px" 
+    width="950px" 
+    height="1600px" 
     src="https://forms.office.com/r/wssL87gKyR?embed=true" 
     frameBorder="0" 
     marginWidth="0" 
     marginHeight="0" 
-    style={{ border: 'none', maxWidth: '100%', maxHeight: '100vh' }} 
+    style={{ border: 'none' }} 
     allowFullScreen 
     webkitAllowFullScreen 
     mozAllowFullScreen 
